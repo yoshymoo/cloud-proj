4add684224e2a9ff26841eb6827dd14352375bfb
Bi Yan <biyantay@gmail.com>
2025-04-04 20:18:29 +0800

    hi

--- src/aws-export.js
+++ src/aws-export.js
@@ -1,11 +1,12 @@
-// src/aws-exports.js
+// src/aws-exports.js (mock version to prevent build error)
 const awsmock = {
   aws_project_region: "us-east-1",
   aws_cognito_region: "us-east-1",
-  aws_user_pools_id: "mock-pool-id",
+  aws_user_pools_id: "mock-user-pool-id",
   aws_user_pools_web_client_id: "mock-client-id",
   aws_appsync_graphqlEndpoint: "https://mock-api/graphql",
   aws_appsync_region: "us-east-1",
   aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
 };
+
 export default awsmock;
