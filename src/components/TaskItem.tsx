7402d7dde812c3f42f1f17cf72c6c5f08f9c8677
Bi Yan <biyantay@gmail.com>
2025-04-04 19:58:55 +0800

    test

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,16 +1,27 @@
-// src/components/TaskItem.tsx
-
 import React from "react";
 
 type TaskItemProps = {
   taskName: string;
   isCompleted: boolean;
+  onToggle: () => void;
+  onDelete: () => void;
 };
 
-const TaskItem: React.FC<TaskItemProps> = ({ taskName, isCompleted }) => {
+const TaskItem: React.FC<TaskItemProps> = ({ taskName, isCompleted, onToggle, onDelete }) => {
   return (
-    <li style={{ textDecoration: isCompleted ? "line-through" : "none" }}>
-      {taskName}
+    <li style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <span
+        style={{
+          textDecoration: isCompleted ? "line-through" : "none",
+          cursor: "pointer",
+        }}
+        onClick={onToggle}
+      >
+        {taskName}
+      </span>
+      <button onClick={onDelete} style={{ marginLeft: "1rem" }}>
+        🗑️
+      </button>
     </li>
   );
 };
